2f909c1b33508cbdfc738c50ec25383914cce928
Estevam Amorim <estevamyusukeamorim@gmail.com>
2022-07-13 02:08:00 -0300

    Add files

---
+++ Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8puzzle
+{
+    public class Program
+    {
+        enum MaxDepth : int
+        {
+            DepthSearch = 24,
+            WidthSearch = 24,
+            GreedySearch = 24,
+            ASearch = 24
+        }
+
+        public static int ASearchMax = (int)Math.Pow(1.4, (double)MaxDepth.ASearch);
+        
+        public static List<(int movement, int reverse)> moves = new List<(int, int)> { (1, 2), (2, 1), (3, 4), (4, 3) };
+
+        public class NextChoice
+        {
+            public TabState choice;
+            public int weight;
+
+            public NextChoice(TabState choice, int weight)
+            {
+                this.choice = choice;
+                this.weight = weight;
+            }
+        }
+        
+        static void DepthSearch(TabState current_state)
+        {   
+            if (current_state.GetDepth() > (int)MaxDepth.DepthSearch || current_state.CheckSolution())
+                return;
+            
+            foreach((int movement, int reverse) move in moves)
+            {
+                if (current_state.GetLast() != move.reverse && current_state.Move(move.movement))
+                {
+                    DepthSearch(current_state);
+
+                    if (current_state.CheckSolution())
+                        return;
+
+                    current_state.Revert();
+                }
+            }
+        }
+
+        static void WidthSearch(TabState tab)
+        {
+            int last;
+            TabState queue_first;
+
+            Queue<TabState> tab_queue = new Queue<TabState>();
+            tab_queue.Enqueue(tab);
+
+            while(tab_queue.Peek().GetDepth() < (int)MaxDepth.WidthSearch)
+            {
+                queue_first = tab_queue.Peek();
+                last = queue_first.GetLast();
+
+                foreach ((int movement, int reverse) move in moves)
+                {
+                    if (last != move.reverse && queue_first.Move(move.movement))
+                    {
+                        if (queue_first.CheckSolution())
+                        {
+                            tab.Update(queue_first);
+                            tab_queue.Clear();
+                            return;
+                        }
+
+                        tab_queue.Enqueue((TabState)queue_first.Clone());
+                        queue_first.Revert();
+                    }
+                }
+
+                tab_queue.Dequeue();
+            }
+
+            tab_queue.Clear();
+        }
+
+        static void GreedySearch(TabState current_state)
+        {
+            int min = 1000;
+            int md, next_move = 0;
+
+            if (current_state.GetDepth() > ASearchMax || current_state.CheckSolution())
+                return;
+            
+            foreach ((int movement, int reverse) move in moves)
+            {
+                if (current_state.GetLast() != move.reverse && current_state.Move(move.movement))
+                {
+                    if (current_state.CheckSolution())
+                        return;
+
+                    md = current_state.ManhattanDistance();
+
+                    if (md < min)
+                    {
+                        min = md;
+                        next_move = move.movement;
+                    }
+                        
+                    current_state.Revert();
+                }
+            }
+
+            current_state.Move(next_move);
+            GreedySearch(current_state);
+        }
+
+        static void ASearch(TabState current_state)
+        {
+            TabState aux = (TabState)current_state.Clone();
+            int weight, index = 0;
+            int value = 9999;
+            List<NextChoice> choices = new List<NextChoice>();
+            
+            while (choices.Count < ASearchMax || current_state.CheckSolution())
+            {
+                if (current_state.GetDepth() < (int)MaxDepth.ASearch)
+                {
+                    foreach ((int movement, int reverse) move in moves)
+                    {
+                        if (current_state.GetLast() != move.reverse && current_state.Move(move.movement))
+                        {
+                            if (current_state.CheckSolution())
+                            {
+                                choices.Clear();
+                                return;
+                            }
+
+                            weight = current_state.ManhattanDistance() + current_state.GetDepth();
+
+                            choices.Add(new NextChoice((TabState)current_state.Clone(), weight));
+                            current_state.Revert();
+                        }
+                    }
+                }
+
+                for (int i = 0; i < choices.Count; i++)
+                {
+                    if (choices[i].weight < value)
+                    {
+                        value = choices[i].weight;
+                        index = i;
+                    }
+                }
+  
+                current_state.Update(choices[index].choice);
+                choices.RemoveAt(index);
+            }
+
+            current_state.Update(aux);
+            choices.Clear();
+        }
+
+        static void Main(string[] args)
+        {
+            TabState.InitSolution(new int[,] { { 1, 2, 3 }, { 4, 0, 5 }, { 6, 7, 8 } });
+
+            //TabState t = new TabState(new int[,] { {1, 2, 3}, {4, 0, 5}, {6, 8, 7} });
+            //TabState t = new TabState(new int[,] { {0, 7, 2 }, { 1, 4, 3 }, { 6, 8, 5 } });
+            TabState t = new TabState(new int[,] { { 4, 1, 2 }, { 0, 5, 3 }, { 6, 7, 8 } });
+            //TabState t = new TabState(new int[,] { { 1, 2, 3 }, { 4, 7, 5 }, { 6, 8, 0 } });
+
+            DepthSearch(t);
+            
+            //WidthSearch(t);
+
+            //GreedySearch(t);
+
+            //ASearch(t);
+
+            //GreedySearch(t);
+
+            Console.WriteLine(t.ToString());
+        }
+    }
+}
---
+++ TabState.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8puzzle
+{
+    //Classe que representa um estado
+    public class TabState : ICloneable
+    {
+        //estado do jogo como uma matriz, 0 é o espaco em branco
+        private int[,] state;
+
+        //coordenadas do espaco em branco
+        private int empty_l;
+        private int empty_c;
+
+        //estado final a ser buscado
+        private static int[,]? solution_template;
+
+        //movimentos realizados para chegar no estado atual, pilha onde o elemento mais ao topo representa
+        //o ultimo movimento. 1- up, 2 - down, 3 - right, 4 - left
+        private Stack<int> path;
+
+        public TabState(int[,] state)
+        {
+            this.state = state;
+            this.path = new Stack<int>();
+            Update0Position();
+        }
+
+        //sobrecarga do construtor para permitir clonagem
+        private TabState(TabState t)
+        {
+            state = (int[,])(t.state).Clone();
+            this.empty_l = t.empty_l;
+            this.empty_c = t.empty_c;
+            this.path = new Stack<int>(new Stack<int>(t.path));
+        }
+
+        public override string ToString()
+        {
+            string result = "{";
+
+            for (int i = 0; i < 3; i++)
+            {
+                result += "{";
+                for (int j = 0; j < 3; j++)
+                {
+                    result += state[i, j].ToString() + ", ";
+                }
+                result += "}, ";
+            }
+
+            return result + "}";
+        }
+
+        public object Clone()
+        {
+            return new TabState(this);
+        }
+
+        public static void InitSolution(int[,] s)
+        {
+            solution_template = new int[9, 2];
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    solution_template[s[i, j], 0] = i;
+                    solution_template[s[i, j], 1] = j;
+                }
+            }
+        }
+
+        //quantidade de elementos em path representa a profundidade do estado na arvore
+        public int GetDepth()
+        {
+            return path.Count;
+        }
+
+        //ultimo movimento usado para chegar no estado
+        public int GetLast()
+        {
+            if (path.Count == 0)
+                return 0;
+
+            return path.Peek();
+        }
+
+        public int ManhattanDistance()
+        {
+            float manh = 0;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    manh += Math.Abs(solution_template[state[i, j], 0] - i) + Math.Abs(solution_template[state[i, j], 1] - j);
+                }
+            }
+
+            return (int)manh;
+        }
+
+        public void Update(TabState t)
+        {
+            this.state = (int[,])(t.state).Clone();
+            this.empty_l = t.empty_l;
+            this.empty_c = t.empty_c;
+            this.path = new Stack<int>(new Stack<int>(t.path));
+        }
+
+        private void Update0Position()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (state[i, j] == 0)
+                    {
+                        empty_l = i;
+                        empty_c = j;
+                    }
+                }
+            }
+        }
+
+        //reverte o ultimo movimento
+        public void Revert()
+        {
+            if (path.Count != 0)
+            {
+                switch (path.Pop())
+                {
+                    case 1:
+                        this.DownMove();
+                        break;
+                    case 2:
+                        this.UpMove();
+                        break;
+                    case 3:
+                        this.LeftMove();
+                        break;
+                    case 4:
+                        this.RightMove();
+                        break;
+                }
+
+                path.Pop();
+            }
+
+        }
+
+        //movimentos, a chamada desses metodos reflete a alteracao no atributo state relativa ao movimento
+        private bool UpMove()
+        {
+            if (empty_l != 0)
+            {
+                state[empty_l, empty_c] = state[empty_l - 1, empty_c];
+                state[empty_l - 1, empty_c] = 0;
+                empty_l--;
+                path.Push(1);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DownMove()
+        {
+            if (empty_l != 2)
+            {
+                state[empty_l, empty_c] = state[empty_l + 1, empty_c];
+                state[empty_l + 1, empty_c] = 0;
+                empty_l++;
+                path.Push(2);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool RightMove()
+        {
+            if (empty_c != 2)
+            {
+                state[empty_l, empty_c] = state[empty_l, empty_c + 1];
+                state[empty_l, empty_c + 1] = 0;
+                empty_c++;
+                path.Push(3);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool LeftMove()
+        {
+            if (empty_c != 0)
+            {
+                state[empty_l, empty_c] = state[empty_l, empty_c - 1];
+                state[empty_l, empty_c - 1] = 0;
+                empty_c--;
+                path.Push(4);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Move(int move)
+        {
+            if (move == 1)
+                return this.UpMove();
+
+            if (move == 2)
+                return this.DownMove();
+
+            if (move == 3)
+                return this.RightMove();
+
+            if (move == 4)
+                return this.LeftMove();
+
+            return false;
+        }
+
+        //verifica se o estado atual eh igual ao estado final
+        public bool CheckSolution()
+        {
+            int value_line, value_col;
+
+            for (int value = 0; value < 9; value++)
+            {
+                value_line = solution_template[value, 0];
+                value_col = solution_template[value, 1];
+
+                if (state[value_line, value_col] != value)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
